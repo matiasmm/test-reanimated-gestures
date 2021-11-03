caf1af2e53605f19b466a27108ad408c95290707
Matias M. Montenegro <matias@analyticsfire.com>
2021-11-03 12:01:00 -0300

    Updated

--- App.js
+++ App.js
@@ -1,66 +1,45 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {SafeAreaView, ScrollView, StyleSheet, useColorScheme, Animated} from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import {PanGestureHandler} from 'react-native-gesture-handler';
 import Reanimated from 'react-native-reanimated';
-import Svg, {Circle} from 'react-native-svg';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
-const AnimatedScrollView = Reanimated.createAnimatedComponent(ScrollView);
 
 const _touchX = new Reanimated.Value(0);
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  const onGestureEvent = () => {
-
-  };
-
-  console.log(46);
+  const onPanGestureEvent = Reanimated.event(
+    [
+      {
+        nativeEvent: {
+          translationX: _touchX,
+        },
+      },
+    ],
+    {
+      useNativeDriver: true,
+    },
+  )
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <PanGestureHandler>
-        <Reanimated.View style={[styles.view, {transform: [{translateX: _touchX}]}]}>
-          <Svg width={100} height={100} style={{backgroundColor: 'green'}}>
-            <Circle x={250} y={250} r={100} fill="red" />
-          </Svg>
-        </Reanimated.View>
+    <View style={styles.container}>
+      <PanGestureHandler onGestureEvent={onPanGestureEvent}>
+        <Reanimated.View style={[styles.view, {transform: [{translateX: _touchX}]}]} />
       </PanGestureHandler>
-      <Svg width={500} height={500} style={{backgroundColor: 'yellow'}}>
-        <Circle x={250} y={250} r={100} fill="red"/>
-      </Svg>
-      <Svg width={500} height={500} style={{backgroundColor: 'pink'}}>
-        <Circle x={250} y={250} r={100} fill="red"/>
-      </Svg>
-      <Svg width={500} height={500} style={{backgroundColor: 'blue'}}>
-        <Circle x={250} y={250} r={100} fill="red"/>
-      </Svg>
-      <Svg width={500} height={500} style={{backgroundColor: 'orange'}}>
-        <Circle x={250} y={250} r={100} fill="red"/>
-      </Svg>
-    </SafeAreaView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'gray',
+    flex: 1,
+  },
   view: {
     marginTop: 32,
     paddingHorizontal: 24,
-  },
-
+    backgroundColor: 'red',
+    width: 200,
+    height: 200,
+  }
 });
 
 export default App;
